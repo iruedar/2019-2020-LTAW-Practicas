5813a4cddd4b575ebfa72c6e320377e53e82ca51
iruedar <nachete.rueda@gmail.com>
2020-02-10 11:51:47 +0100

    procesando errores

--- P1/fs_test1.js
+++ P1/fs_test1.js
@@ -1,15 +1,16 @@
-const fs = require('fs');
+var fs = require('fs');
 
-console.log("Este mensaje está AL COMIENZO del código")
-
-//-- Leer el fichero. Al terminar se invoca a la función show_file
-fs.readFile('test.txt', 'utf8', show_file);
-
-//-- Funcion llamada cuando se ha terminado de leer el fichero
-function show_file(err, data) {
-    console.log("---> Comienzo del fichero leido")
-    console.log(data)
-    console.log("---> Final del fichero")
-}
-
-console.log("Este mensaje está al FINAL del código")
+//-- Leer el fichero test2.txt, que NO existe
+fs.readFile('test2.txt', 'utf8', (err, data) => {
+    if (err) {
+      console.log()
+      console.log("-------> ERROR!!")
+      console.log(err.message)
+      console.log()
+    }
+    else { //-- Lectura normal, cuando no hay errores
+      console.log("---> Comienzo del fichero leido")
+      console.log(data)
+      console.log("---> Final del fichero")
+    }
+});
