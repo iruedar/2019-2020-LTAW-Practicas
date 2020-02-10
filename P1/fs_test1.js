c22a7d8b228c7b5da1219e0423a3295016db7ee0
iruedar <nachete.rueda@gmail.com>
2020-02-10 11:43:49 +0100

    comprobación de eventos

--- P1/fs_test1.js
+++ P1/fs_test1.js
@@ -1,12 +1,15 @@
-// Acceso al módulo fs, para lectura de ficheros
 const fs = require('fs');
 
-//-- Funcion llamada cuando se ha terminado de leer el fichero
-function show_file(err, data) {
-
-    //-- Mostrar el contenido del fichero
-    console.log(data)
-}
+console.log("Este mensaje está AL COMIENZO del código")
 
 //-- Leer el fichero. Al terminar se invoca a la función show_file
 fs.readFile('test.txt', 'utf8', show_file);
+
+//-- Funcion llamada cuando se ha terminado de leer el fichero
+function show_file(err, data) {
+    console.log("---> Comienzo del fichero leido")
+    console.log(data)
+    console.log("---> Final del fichero")
+}
+
+console.log("Este mensaje está al FINAL del código")
