855197ce8b3d23604ed6caf4f1d664986ffc37d9
iruedar <nachete.rueda@gmail.com>
2020-02-19 16:47:53 +0100

    Empezando la tienda

---
+++ P1/Tienda/server.js
@@ -0,0 +1,44 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const PUERTO = 8080
+
+//-- Configurar y lanzar el servidor. Por cada peticion recibida
+//-- se imprime un mensaje en la consola
+http.createServer((req, res) => {
+  console.log("----------> Peticion recibida")
+  let q = url.parse(req.url, true);
+  console.log("Recurso:" + q.pathname)
+
+
+  let filename = ""
+
+  //-- Obtener fichero a devolver
+  if (q.pathname == "/"){
+    filename += "index.html"
+  } else {
+    filename = q.pathname.substr(1)
+  }
+
+  //-- Leer fichero
+  fs.readFile(filename, function(err, data) {
+
+    //-- Fichero no encontrado. Devolver mensaje de error
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    }
+
+    //-- Tipo mime por defecto: html
+    let mime = ""
+
+    //-- Generar el mensaje de respuesta
+    res.writeHead(200, {'Content-Type': mime});
+    res.write(data);
+    res.end();
+  });
+
+}).listen(PUERTO);
+
+console.log("Servidor corriendo...")
+console.log("Puerto: " + PUERTO)
