4c0834818d272a0f4c79c7e7e2ca54620d943ec1
iruedar <nachete.rueda@gmail.com>
2020-02-19 15:55:43 +0100

    url-02

--- P1/url-02.js
+++ P1/url-02.js
@@ -7,17 +7,17 @@ http.createServer( (req, res) => {
   console.log("---> Peticion recibida")
   console.log("Recurso solicitado (URL): " + req.url)
 
-  //-- Analisis de la url recibida:
+  //-- Analisis de la URL recibida:
   let q = url.parse(req.url, true);
 
-  console.log("Pathname: " + q.pathname)
-  console.log("Search: " + q.search)
-  console.log("Busqueda: ")
+  console.log("Pathname: " +  q.pathname)
+  console.log("search: " + q.search)
+  console.log("Búsqueda:")
   let qdata = q.query
   console.log(qdata)
 
   //-- Acceso al objeto
-  console.log("Articulo: " + qdata.articulo)
+  console.log("Artículo: " + qdata.articulo)
   console.log("Color: " + qdata.color)
 
 }).listen(PUERTO);
