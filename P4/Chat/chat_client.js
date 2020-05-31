87e953422ed57d8bc4d723922589f31b819ccf4f
iruedar <nachete.rueda@gmail.com>
2020-05-31 17:37:04 +0200

    p4 acabada

---
+++ P4/Chat/chat_client.js
@@ -0,0 +1,46 @@
+console.log("Ejecutando cliente JS...");
+
+//-- Obtener los elementos del DOM
+const display = document.getElementById("display");
+const msg = document.getElementById("msg");
+const send = document.getElementById("send");
+const name = document.getElementById("name_client");
+const register = document.getElementById("reg_client");
+var user = prompt("¿Cuál es tu nombre?");
+
+//-- Crear un websocket. Se establece la conexión con el servidor
+const socket = io();
+
+//-- Se ha recibido el evento 'hello':
+//-- Es el mensaje de bienvenida del servidor
+socket.on('hello', (msg) => {
+  //-- Mostrarlo en la consola del navegador, para
+  //-- depurar
+  console.log("Mensaje del servidor: " + msg);
+  //-- Ponerlo en el párrafo display
+  display.innerHTML = msg;
+});
+
+socket.emit('name', user);
+
+//-- Se ha recibido un mensaje
+socket.on('msg', (msg) => {
+  //-- Añadirlo al párrafo display
+  display.innerHTML += "<br> > " + msg;
+});
+
+//-- Botón de envío mensaje apretado
+send.onclick = () => {
+  //-- Se envía el mensaje escrito
+  //-- Usamos el nombre 'msg' para los mensajes de usuario
+  //-- Si no se ha introducido ningún mensaje, no se envía
+  if (msg.value){
+    if (msg.value[0] == '/'){
+      socket.emit('cmd', msg.value);
+    }else {
+      socket.emit('msg', msg.value);
+    }
+  }
+  //-- Borramos el mensaje escrito
+  msg.value="";
+}
